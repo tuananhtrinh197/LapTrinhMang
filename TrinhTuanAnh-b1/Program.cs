1e9b22333c42416f74ae764d7a0e0f1ceb46e08a
TuanAnh <TuanAnh@DESKTOP-LJRVQIE>
2017-10-03 01:10:03 +0700

    Add project files.

---
+++ TrinhTuanAnh-b1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace TrinhTuanAnh_b1
+{
+    class classGetIPCS
+    {
+        ///
+        /// Gets IP addresses of the local machine
+        ///
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Get host name
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Enumerate IP addresses
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                                  ipaddress.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ bai2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace bai2
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien hoac ip: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+
+            // In tên miền
+            foreach (string s in e.Aliases)
+                Console.WriteLine("\t+ Alias: {0} \n", s);
+
+            // Danh sách điạ chỉ IP
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t+ IP: {0}", i);
+            Console.ReadKey();
+        }
+    }
+        
+  }
+
