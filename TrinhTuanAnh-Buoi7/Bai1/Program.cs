4f330e247e41f1aa6a2bac21503b284ecf83d247
Anh Trinh Tuan <14dh110129@st.huflit.edu.vn>
2017-11-17 10:37:05 +0700

    commit

--- TrinhTuanAnh-Buoi7/Bai1/Program.cs
+++ TrinhTuanAnh-Buoi7/Bai1/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bai1
 {
-    class Program
-    {
-        static void Main(string[] args)
+   
+        
+            class Server
         {
+            static Socket listen;
+            static Socket client;
+            static IPEndPoint ipe;
+            static int connections = 0;
+            public static void Main()
+            {
+                listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                ProtocolType.Tcp);
+                ipe = new IPEndPoint(IPAddress.Any, 9050);
+                listen.Bind(ipe);
+                listen.Listen(10);
+                Console.WriteLine("Waiting for clients...");
+                while (true)
+                {
+                    if (listen.Poll(1000000, SelectMode.SelectRead))
+                    {
+                        client = listen.Accept();
+                        Thread newthread = new Thread(new ThreadStart(HandleConnection));
+                        newthread.Start();
+                    }
+                }
+            }
+            static void HandleConnection()
+            {
+                int recv;
+                byte[] data = new byte[1024];
+                NetworkStream ns = new NetworkStream(client);
+                connections++;
+                Console.WriteLine("New client accepted: {0} active connections",
+                connections);
+                string welcome = "Welcome to my test server";
+                data = Encoding.ASCII.GetBytes(welcome); ns.Write(data, 0, data.Length);
+                while (true)
+                {
+                    data = new byte[1024];
+                    recv = ns.Read(data, 0, data.Length);
+                    if (recv == 0)
+                        break;
+                    ns.Write(data, 0, recv);
+                }
+                ns.Close();
+                client.Close();
+                connections--;
+                Console.WriteLine("Client disconnected: {0} active connections",
+                connections);
+            }
         }
     }
-}
+    
+
---
+++ TrinhTuanAnh-Buoi7/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    
+        {
+            static void Main(string[] args)
+            {
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                ProtocolType.Tcp);
+                IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+                byte[] data = new byte[1024];
+                string stringData;
+                int recv;
+                sock.Connect(ipe);
+                Console.WriteLine("Connected to server");
+                recv = sock.Receive(data);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("Received: {0}", stringData);
+                while (true)
+                {
+                    stringData = Console.ReadLine();
+                    if (stringData == "exit")
+                        break;
+                    data = Encoding.ASCII.GetBytes(stringData);
+                    sock.Send(data, data.Length, SocketFlags.None);
+                    data = new byte[1024];
+                    recv = sock.Receive(data);
+                    stringData = Encoding.ASCII.GetString(data, 0, recv);
+                    Console.WriteLine("Received: {0}", stringData);
+                }
+                sock.Close();
+            }
+        }
+    }
+
