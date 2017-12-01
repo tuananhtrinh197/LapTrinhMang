4dd20fc3d47301cbc767d5bcb6c6b3fa87ca6a31
Anh Trinh Tuan <14dh110129@st.huflit.edu.vn>
2017-12-01 10:27:04 +0700

    Commit

---
+++ TrinhTuanAnh-Buoi9/SendEmail/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmail
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SmtpClient mailclient = new SmtpClient("smtp.gmail.com", 587);
+            mailclient.EnableSsl = true;
+            mailclient.Credentials = new NetworkCredential("abc123@gmail.com",
+           "123");
+            MailMessage message = new MailMessage("abc@gmail.com" ,
+           "xyz@gmail.com");
+            message.Subject = "Hello!";
+            message.Body = "Im abc!";
+            mailclient.Send(message);
+            Console.WriteLine("Mail sent successfully!");
+        }
+    }
+}
